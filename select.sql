6216247c3862d0f71b468c5bed852c414646b576
NethraSuman <162802765+NethraSuman@users.noreply.github.com>
2024-05-10 16:22:52 +0530

    Add files via upload

--- select.sql
+++ select.sql
@@ -1,5 +1,24 @@
+-- SELECT statement
+
+-- select all (*), specific columns from the given 15 tables
 SELECT * FROM actor;
 SELECT first_name, last_name FROM actor;
 SELECT last_name, first_name FROM actor;
+SELECT * FROM address;
+SELECT address, district, phone FROM address;
+SELECT * FROM category;
+SELECT * FROM city;
+SELECT * FROM country;
+SELECT * FROM customer;
+SELECT first_name, last_name FROM customer;
 SELECT * FROM film;
 SELECT title, description, rating FROM film;
+SELECT * FROM film_actor;
+SELECT * FROM film_category;
+SELECT * FROM inventory;
+SELECT * FROM language;
+SELECT * FROM payment;
+SELECT * FROM rental;
+SELECT * FROM staff;
+SELECT * FROM store;
+-- End of SELECT statement
